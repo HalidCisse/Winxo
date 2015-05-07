e6da5e3f5537dd4e9671b1130108cf99c1aeedf9
Halid Cisse <halidoucse@hotmail.com>
2015-05-07 13:57:45 +0100

    Working On  Home Page

---
+++ Winxo/Views/ComingSoon.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Winxo.Views
+{
+    
+    public partial class ComingSoon 
+    {
+        public ComingSoon()
+        {
+            InitializeComponent();
+        }
+
+        private void BACK_BUTTON_OnClick(object sender, RoutedEventArgs e) =>
+           new Task(() => { Dispatcher.BeginInvoke(new Action(() => { NavigationService?.GoBack(); })); }).Start();
+    }
+}
---
+++ Winxo/Views/HomeView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+
+
+namespace Winxo.Views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home 
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        #region Home Buttons Commandes
+
+        private void StudentButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        private void PedagogieButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        private void StaffButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        private void AgendaButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        private void EconomatButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        private void StatisticButton_Click(object sender, RoutedEventArgs e) => new Task(() => {
+            Dispatcher.BeginInvoke(new Action(()
+=> { NavigationService?.Navigate(new ComingSoon(), UriKind.Relative); }));
+        }).Start();
+
+        #endregion
+    }
+}
